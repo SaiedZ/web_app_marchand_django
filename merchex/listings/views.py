41823ee70b3a3dee622aa223b1aa11b3b2fb9b86
SaiedZ <saied_am@hotmail.com>
2022-01-11 15:53:34 +0100

    création des vues et d'un model Band

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
 
-# Create your views here.
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
+
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1><p>Nous adorons merch!</p>')
+
+
+def listings(request):
+    return HttpResponse('<h1>Annonces</h1><p>La liste des annonces</p>')
+
+
+def contact(request):
+    return HttpResponse('<h1>Contactez nous</h1><p>Formulaire</p>')
